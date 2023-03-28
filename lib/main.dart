067a92d96fd31b0cf00943c236c5a6eca989c7ac
SayedNawid <sayednawidmohammady@gmail.com>
2023-03-28 09:26:35 +0430

    firstCommit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.lightBlue,
+        appBar: AppBar(
+          title: Center(child: Text("I am Rich")),
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/asd.jpg'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
