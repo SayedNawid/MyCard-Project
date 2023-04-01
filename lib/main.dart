acf8c6eadf6e5779d1117c962639a89372620a36
SayedNawid <sayednawidmohammady@gmail.com>
2023-04-02 00:25:20 +0430

    Finished MyCard Project

--- lib/main.dart
+++ lib/main.dart
@@ -18,33 +18,79 @@ class MyWidget extends StatelessWidget {
           backgroundColor: Color.fromARGB(255, 90, 117, 121),
         ),
         body: SafeArea(
-            child: Column(
-          children: [
-            CircleAvatar(
-              radius: 50,
-              backgroundImage: AssetImage('images/asd.jpg'),
-            ),
-            Text(
-              "Mr.CatAvatar",
-              style: TextStyle(
-                fontFamily: 'Pacifico',
-                fontSize: 40,
-                fontWeight: FontWeight.bold,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/asd.jpg'),
+              ),
+              Text(
+                "Mr.CatAvatar",
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                "FLUTTER DEVELOPER",
+                style: TextStyle(
+                  fontFamily: "Source Sans Pro",
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5,
+                  color: Colors.teal[100],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.blueGrey.shade900,
+                ),
+              ),
+              Card(
                 color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                      Icons.phone,
+                      size: 30,
+                      color: Colors.blueGrey,
+                    ),
+                    title:  Text(
+                      "+(93) 792 881 775",
+                      style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20,
+                        color: Colors.teal.shade900,
+                      ),
+                    ),
+                )
               ),
-            ),
-            Text(
-              "FLUTTER DEVELOPER",
-              style: TextStyle(
-                fontFamily: "Source Sans Pro",
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 2.5,
-                color: Colors.teal[100],
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                      Icons.email,
+                      color: Colors.blueGrey,
+                      size: 30,
+                    ),
+                    title: Text(
+                      "sayednawidmohammady@gmail.com",
+                      style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        color: Colors.teal.shade900,
+                      ),
+                    ), 
+                )
               ),
-            ),
-          ],
-        )),
+            ],
+          ),
+        ),
       ),
     );
   }
